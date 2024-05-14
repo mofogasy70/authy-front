56a95e535eab7a15d08629f502949752dd4a2b26
mofogasy70 <99830988+mofogasy70@users.noreply.github.com>
2024-05-14 18:00:40 +0300

    soket

--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -3,8 +3,11 @@ import { Navigate } from "react-router-dom";
 import Sidebar from "../Component/Sidebar";
 import Navbar from "../Component/navbar/Navbar";
 import userService from "../service/user/userService";
-import { Spin } from "antd";
+import { Spin, message } from "antd";
 import Foot from "../Component/Foot";
+import jwtDecode from "jwt-decode";
+import { VITE_URIAPI } from "../constant";
+import { io } from "socket.io-client";
 
 interface IsubMenu {
   title: string,
@@ -24,6 +27,30 @@ const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ element }) => {
   const [isValid, setIsValid] = useState<boolean>(false);
   const [loading, setLoading] = useState<boolean>(true);
   const [Sidebartab, setSidebartab] = useState<IMenu[]>([]);
+  const token: any = localStorage.getItem('token');
+  const decode: any = jwtDecode(token);
+  const socket = io(VITE_URIAPI);
+  const [messageApi, contextHolder] = message.useMessage();
+  async function notification(type: 'success' | 'error', message: string) {
+    await messageApi.open({
+      type: type,
+      content: message,
+      duration: 1.5,
+    });
+  }
+  useEffect(() => {
+    console.log(String(decode.UserId));
+    socket.on("test", (data: any) => { 
+      notification(data.type, data.message);
+    });
+    return () => {
+      socket.off("test");
+    };
+  }, [])
+  const test = () => {
+    socket.emit('notification', { type: "success", message: "mety", UserId: "663a0d099aa6f42ace6861f3" });
+    console.log({ type: "success", message: "mety", UserId: "663a0d099aa6f42ace6861f3" });
+  }
   useEffect(() => {
     const checkTokenValidity = async () => {
       const token = localStorage.getItem('token');
@@ -57,18 +84,20 @@ const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ element }) => {
   }
   return (
     <div>
-    <div className="flex min-h-min w-full ">
-      <Sidebar menumode={menumode} menu={Sidebartab} setMenumode={setMenumode} />
-      <div className="w-full h-auto bg-[#EFF3F4]">
-        <Navbar menumode={menumode} setMenumode={setMenumode} />
-        <div className="flex w-full min-h-min  items-center justify-center">
-          <div className="w-full md:w-11/12 min-h-min">
-            {element}
+      {contextHolder}
+      <div className="flex min-h-min w-full ">
+        <Sidebar menumode={menumode} menu={Sidebartab} setMenumode={setMenumode} />
+        <div className="w-full h-auto bg-[#EFF3F4]">
+          <Navbar menumode={menumode} setMenumode={setMenumode} />
+          <div className="flex w-full min-h-min  items-center justify-center">
+            <div className="w-full md:w-11/12 min-h-min">
+              {element}
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    <Foot></Foot>
+      <button onClick={test}>test</button>
+      <Foot />
     </div>
   );
 };
